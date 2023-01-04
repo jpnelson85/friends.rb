08f5a56fbf8a3bba955d12d65f7885b9a89ccbcf
Jeff Nelson <jpnelson85@gmail.com>
2023-01-04 17:20:24 -0600

    add places

--- friends.rb
+++ friends.rb
@@ -3,3 +3,8 @@ arrays = ["jim", "jack", "jill", "jeff", "julia"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["indiana", "denver", "oklahoma", "arkansas", "canada"]
+
+puts places.sort
+
